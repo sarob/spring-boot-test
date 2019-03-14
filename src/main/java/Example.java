4e516471daeacbddb43bd5301640754f5eb749a8
sarob <seanroberts66@gmail.com>
2019-03-14 16:15:09 -0700

    add example code

---
+++ src/main/java/Example.java
@@ -0,0 +1,18 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController /*web controller*/
+@EnableAutoController /*guess how to configure spring*/
+public class Example {
+		@RequestMapping("/") /*routing for HTTP / request mapped to home method */
+		String home() {
+			return "Hello World!";
+		}
+		
+		public static void main(String[] args) {
+			SpringApplication.run(Example.class, args);/*bootstraps app, 
+			starts spring, then tomcat, pass Example.class to the run method 
+			of SpringApplication with args*/
+		}
+}
